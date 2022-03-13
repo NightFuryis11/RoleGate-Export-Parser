bb513645a45ca40e2c63a70f0459df9dfba3e2aa
NightFuryis11 <catiosforcats@gmail.com>
2022-03-13 07:55:37 +0000

    Small changes

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## RoleGate Export Parser
 
 Ever want to save your RoleGate games for posterity? Ever want to cleanly read the story you've created without having to load up the site? Then the RGExP is for you!
+
+Create an account via this link to give me credit towards an account upgrade! *wink*
+https://www.rolegate.com/ref/NightFuryis11
--- RGExP/main.py
+++ RGExP/main.py
@@ -17,10 +17,13 @@ class Message(object):
         "name":""
     }
     tone = ''
+    action = ''
     created = ''
     whisperedto = []
     isdescription = False
     isstorytelling = False
+    result = []
+    mtype = ''
     
     def __init__(self, dictionary):
         self.mid = dictionary['mid']
@@ -31,9 +34,12 @@ class Message(object):
         self.name = dictionary['name']
         self.tone = dictionary['tone']
         self.whisperedto = dictionary['whisperedto']
+        self.action = dictionary['action']
         self.created = dictionary['created']
         self.isdescription = dictionary['isdescription']
         self.isstorytelling = dictionary['isstorytelling']
+        self.result = dictionary['result']
+        self.mtype = dictionary['mtype']
         
     def read(self):
         values = {
@@ -45,17 +51,18 @@ class Message(object):
             'name':self.name,
             'tone':self.tone,
             'whisperedto':self.whisperedto,
+            'action':self.action,
             'created':self.created,
             'isdescription':self.isdescription,
-            'isstorytelling':self.isstorytelling
+            'isstorytelling':self.isstorytelling,
+            'result':self.result,
+            'mtype':self.mtype
         }
         return values
 
 def NewMessage(dictionary):
     message = Message(dictionary)
     return message
-    
-# Roll class
 
 message = {}
 Messages = []
@@ -71,9 +78,10 @@ for party in file: # Each party has a party and a content division. This splits 
                     partyid = None
             elif key == 'content': # Here we parse the actual content of the parties. Each 'field' is a message or a roll.
                 for field in values:
-                    if limiter > 20:
-                        break
+                    #if limiter > 202:
+                    #    break
                     if isinstance(field, dict):
+                        rollRes = []
                         message.update({
                             'mid':f"{field.setdefault('id', None)}",
                             'pid':f"{partyid}",
@@ -96,20 +104,74 @@ for party in file: # Each party has a party and a content division. This splits 
                                 'sheet':None,
                                 'name':None
                             })
+                        if 'action' in field:
+                            for a in field['action']:
+                                message.update({
+                                    'action':field['action'][a]
+                                })
+                        else:
+                            message.update({
+                                'action':None
+                            })
+                        if 'pools' in field:
+                            for p in field['pools']:
+                                if 'final_result' in p:
+                                    rollRes.append(p['final_result'])
+                                    message.update({
+                                        'result':rollRes
+                                    })
+                                else:
+                                    for z in p['rolls']:
+                                        if 'modifier' in p:
+                                            for val in z['final_result']:
+                                                vals = val + p['modifier']
+                                                rollRes.append(vals)
+                                                message.update({
+                                                    'result':rollRes
+                                                })
+                                        else:
+                                            rollRes.append(z['final_result'])
+                                            message.update({
+                                                'result':rollRes
+                                            })
+                            message.update({
+                                'mtype':'roll'
+                            })
+                                        #print(message['mid'], (q['final_result'] + p['rolls']['modifier']), message['created'], limiter)
+                                        
+                                
+                        else:
+                            message.update({
+                                'result':None,
+                                'mtype':'message'
+                            })
+                        
                     m = NewMessage(message)
                     Messages.append(m)
                     limiter += 1
 mss = 0
 Messages.sort(key=attrgetter('mid'))
-with open("printout.txt", "w") as p:
+with open("printout.txt", "w") as i:
     while mss < len(Messages):
-        r = Messages[mss]
-        if r.aid == None:
-            p.writelines(f"     Narrator (description): {r.content}\n")
-        elif r.isdescription == True:
-            p.writelines(f"     {r.name} (description): {r.content}\n")
-        elif r.tone != "None":
-            p.writelines(f"     {r.name} ({r.tone}): {r.content}\n")
-        else:
-            p.writelines(f"     {r.name}: {r.content}\n")
+        e = Messages[mss]
+        if e.mtype == 'message':
+            if e.aid == None:
+                i.writelines(f"{mss}     Narrator (description): {e.content}\n")
+            elif e.isdescription == True:
+                i.writelines(f"{mss}     {e.name} (description): {e.content}\n")
+            elif e.tone != "None":
+                i.writelines(f"{mss}     {e.name} ({e.tone}): {e.content}\n")
+            else:
+                i.writelines(f"{mss}     {e.name}: {e.content}\n")
+        elif e.mtype == 'roll':
+            if e.aid == None:
+                if e.action != None:
+                    i.writelines(f"{mss}     <Roll> Narrator rolls {e.content} as '{e.action}' with final result of {str(e.result).replace('[','').replace(']','')}.\n")
+                else:
+                    i.writelines(f"{mss}     <Roll> Narrator rolls {e.content} with final result of {str(e.result).replace('[','').replace(']','')}.\n")
+            else:
+                if e.action != None:
+                    i.writelines(f"{mss}     <Roll> {e.name} rolls {e.content} as '{e.action}' with final result of {str(e.result).replace('[','').replace(']','')}.\n")
+                else:
+                    i.writelines(f"{mss}     <Roll> {e.name} rolls {e.content} with final result of {str(e.result).replace('[','').replace(']','')}.\n")
         mss += 1
